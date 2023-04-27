de0dbeb60511d44afe4121e829e681c9c152b972
Taylor Howell <howelltaylor195@gmail.com>
2023-04-27 14:55:19 -0500

    adding poke rank component

---
+++ src/components/PokeRankList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex items-center flex-wrap md:grid grid-cols-3 gap-4 my-2">
+    <div class="col-span-2">
+      <input
+        type="text"
+        class="p-2 rounded-lg md:w-full text-white input input-bordered max-w-xs"
+        placeholder="Search Pokemon..."
+        v-model="searchPokemonName"
+      />
+      <button @click="$emit('fetchPokemonRank')" class="btn btn-primary text-white shadow mx-2">
+        <!-- <RouterLink to="/register">Search</RouterLink> -->
+        Search
+      </button>
+    </div>
+    <div class="flex justify-end px-2 my-auto">
+      <!-- <RouterLink to="/register">Create Account</RouterLink> -->
+    </div>
+  </div>
+  <div class="w-full">
+    <div v-if="!loadingPokemonRank">
+      <div
+        v-for="p in PokeAccountRank"
+        :key="p"
+        class="grid grid-cols-3 w-full p-2 bg-gray-800 text-white m-1 rounded"
+      >
+        <p>{{ p.Rank }}</p>
+        <p>{{ p.Username }}</p>
+        <p class="text-right">{{ p.PokemonCount }}</p>
+      </div>
+    </div>
+    <div v-else>
+      <LoadingSpinnerView />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+import LoadingSpinnerView from '../components/LoadingSpinnerView.vue'
+
+defineProps(['PokeAccountRank', 'loadingPokemonRank'])
+
+defineEmits(['fetchPokemonRank'])
+</script>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -32,41 +32,13 @@
       <div>
         <AccountsListVue :accounts="accounts" v-if="selectedTab === 0" />
         <div v-if="selectedTab === 1">
-          <div class="flex items-center flex-wrap md:grid grid-cols-3 gap-4 my-2">
-            <div class="col-span-2">
-              <input
-                type="text"
-                class="p-2 rounded-lg md:w-full text-white input input-bordered max-w-xs"
-                placeholder="Search Pokemon..."
-                v-model="searchPokemonName"
-              />
-              <button @click="fetchPokemonRank" class="btn btn-primary text-white shadow mx-2">
-                <!-- <RouterLink to="/register">Search</RouterLink> -->
-                Search
-              </button>
-            </div>
-            <div class="flex justify-end px-2 my-auto">
-              <!-- <RouterLink to="/register">Create Account</RouterLink> -->
-            </div>
-          </div>
-          <div class="w-full">
-            <div v-if="!loadingPokemonRank">
-              <div
-                v-for="p in PokeAccountRank"
-                :key="p"
-                class="grid grid-cols-3 w-full p-2 bg-gray-800 text-white m-1 rounded"
-              >
-                <p>{{ p.Rank }}</p>
-                <p>{{ p.Username }}</p>
-                <p class="text-right">{{ p.PokemonCount }}</p>
-              </div>
-            </div>
-            <div v-else>
-              <LoadingSpinnerView />
-            </div>
-          </div>
+          <PokeRankList
+            :PokeAccountRank="PokeAccountRank"
+            @fetchPokemonRank="fetchPokemonRank"
+            :loadingPokemonRank="loadingPokemonRank"
+          />
+          <div v-if="selectedTab === 2">test2</div>
         </div>
-        <div v-if="selectedTab === 2">test2</div>
       </div>
     </div>
   </div>
@@ -92,6 +64,8 @@ import AccountsListVue from '../components/AccountsList.vue'
 import FullScreenLoading from '../components/FullScreenLoading.vue'
 import YesNoModal from '../components/YesNoModal.vue'
 import LoadingSpinnerView from '../components/LoadingSpinnerView.vue'
+
+import PokeRankList from '../components/PokeRankList.vue'
 
 const user = useUserStore().user
 
